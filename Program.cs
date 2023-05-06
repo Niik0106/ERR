d333e3df8952b423a9283ee405fe0be37b6f127d
Jecy2305 <Edu_Cordova1@hotmail.com>
2023-05-06 14:10:58 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ERR
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese un numero: ");
+            int n = int.Parse(Console.ReadLine());
+            int resultado = factorial_recursivo(n);
+            Console.WriteLine("El factorial de {0} es {1}", n, resultado);
+ 
+        }
+
+        public static int factorial_recursivo(int n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+
+            return n * factorial_recursivo(n - 1);
+            
+        }
+    }
+}
